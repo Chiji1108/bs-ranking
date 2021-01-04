d3fd5b2fadcd90c8ad5c9dc7a5ed7da060524e17
Chiji1108 <shingo.pad@gmail.com>
2021-01-05 01:09:25 +0900

    add adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>ブロスタランキング</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script
+          data-ad-client="ca-pub-4184670726358678"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
