b9ee3a56128b874fc12d8c3ef69c67a5930fddbe
Chiji1108 <shingo.pad@gmail.com>
2021-01-06 12:52:26 +0900

    use env

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,21 +17,12 @@ const client = new ApolloClient({
 });
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
-};
-
 export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined" && !firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
+      firebase.initializeApp(
+        JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!)
+      );
       firebase.analytics();
     }
   }, []);
